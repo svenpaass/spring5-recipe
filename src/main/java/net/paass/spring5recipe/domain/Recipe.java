d6a38f2dabc33e0212e4cec3227c0fee7864adf6
Sven Paaß <sven@paass.net>
2019-04-27 17:22:08 +0200

    one-to-many-jpa relationship

--- src/main/java/net/paass/spring5recipe/domain/Recipe.java
+++ src/main/java/net/paass/spring5recipe/domain/Recipe.java
@@ -1,11 +1,13 @@
 package net.paass.spring5recipe.domain;
 
+import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -25,6 +27,9 @@ public class Recipe {
   // todo add
   // private Difficulty difficulty;
   private Integer rating;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+  private Set<Ingredient> ingredients;
 
   @Lob
   private Byte[] image;
@@ -118,5 +123,13 @@ public class Recipe {
 
   public void setNotes(Notes notes) {
     this.notes = notes;
+  }
+
+  public Set<Ingredient> getIngredients() {
+    return ingredients;
+  }
+
+  public void setIngredients(Set<Ingredient> ingredients) {
+    this.ingredients = ingredients;
   }
 }
